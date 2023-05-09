cdbef24aebfe778565bb0b4304b18f9cee7d8fdb
Kairav Thakkar <87309499+parzivalkairav@users.noreply.github.com>
2023-05-09 15:16:14 +0530

    Create user.js

---
+++ routes/user.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+// Route for user registration
+router.post('/register', (req, res) => {
+  // Handle registration logic here
+});
+
+// Route for user login
+router.post('/login', (req, res) => {
+  // Handle login logic here
+});
+
+// Route for displaying user profile
+router.get('/profile', (req, res) => {
+  // Handle profile display logic here
+});
+
+module.exports = router;
