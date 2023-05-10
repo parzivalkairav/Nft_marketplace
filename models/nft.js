d83f2a6ee4e72f17240f2d54c07e5fae362f73fe
Kairav Thakkar <87309499+parzivalkairav@users.noreply.github.com>
2023-05-10 21:47:27 +0530

    Update nft.js

--- models/nft.js
+++ models/nft.js
@@ -1,1 +1,31 @@
+const mongoose = require('mongoose');
 
+const nftSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('NFT', nftSchema);
